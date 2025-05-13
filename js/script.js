9ed909fe76d80417d921398a67e21857d230ae45
らぐち <jugedred@gmail.com>
2025-05-13 23:41:13 +0900

    最初のコミット

---
+++ js/script.js
@@ -0,0 +1,18 @@
+jQuery(function () {
+  jQuery("#js-button-drawer").on("click", function () {
+    jQuery(this).toggleClass("is-checked");
+    jQuery("#js-drawer").slideToggle();
+    jQuery("body").toggleClass("is-fixed");
+  });
+  jQuery(window).resize(function () {
+    if (jQuery(window).width() >= 768 && jQuery("#js-drawer").css("display") == "none") {
+      jQuery("#js-drawer").css("display", "block");
+    }
+  });
+  jQuery(".header__nav-link").each(function () {
+    $(this).on("click", function () {
+      jQuery("#js-drawer").hide();
+      jQuery("#js-button-drawer").removeClass("is-checked");
+    });
+  });
+});
